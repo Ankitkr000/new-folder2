a98faf677d30076682e69af38c9ccd1b13344269
Ankitkr <ankitkr1001a@gmail.com>
2025-02-03 19:54:48 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://ankitkr1001a:5jCCB6M2TtEAJT79@cluster0.5z0g2.mongodb.net/")
+
+const db=mongoose.connection
+
+module.exports=db
 
 
