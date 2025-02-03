24fd0c2cde4a707498dd2e73e9aa974979ebbd1e
Ankitkr <ankitkr1001a@gmail.com>
2025-02-03 19:29:13 +0530

    done

--- app.js
+++ app.js
@@ -4,25 +4,30 @@ const app=express()
 const userModel=require("./models/userModel")
 
 app.post("/signup",async (req,res)=>{
-    const {name,Email,Password,Username}=req.body
+    try{
+            const {name,Email,Password,Username}=req.body;
 
-    if(Password.length>16 || Password.length<4){
-        res.status(400).send("Password must be under 16")
-    }
+            if(Password.length>16 || Password.length<4){
+                res.status(400).send("Password must be under 16")
+            }
 
-    if(!name){
-        res.status(400).send("Name field cannot be empty")
-    }
-    if(!Username){
-        res.status(400).send("Username field cannot be empty")
-    }
-    if(!Email){
-        res.status(400).send("Email field cannot be empty")
-    }
+            if(!name){
+                res.status(400).send("Name field cannot be empty")
+            }
+            if(!Username){
+                res.status(400).send("Username field cannot be empty")
+            }
+            if(!Email){
+                res.status(400).send("Email field cannot be empty")
+            }
 
-    const newUser=new userModel({name,Email,Password,Email})
+            const newUser=new userModel({name,Email,Password,Email})
 
-    await newUser.save()
+            await newUser.save()
+}catch (err){
+    res.send(err.message)
+    
+}
 
 
 })
